45c303b20e59a4c3686dffde29837ffd1a16d274
周少停 <zst@9elelphas.com>
2017-11-18 13:52:54 +0800

    Initial Commit

---
+++ QRcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRcode
+//
+//  Created by Shaoting Zhou on 2017/11/18.
+//  Copyright © 2017年 Shaoting Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRcode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRcode
+//
+//  Created by Shaoting Zhou on 2017/11/18.
+//  Copyright © 2017年 Shaoting Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
